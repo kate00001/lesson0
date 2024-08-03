c847215530981a9eebc81f2a7a477edd17bb5f4b
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-03 18:20:31 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,9 @@
+# 3rd program
+
+result1 = 2 * 2 + 2
+print(result1)
+
+result2 = 2 * (2 + 2)
+print(result2)
+
+print(result1 == result2)
---
+++ task4.py
@@ -0,0 +1,5 @@
+# 4th program
+# print(type('123.456'))
+# print(type(123.456))
+print((int(float('123.456') * 10) % 10))
+
